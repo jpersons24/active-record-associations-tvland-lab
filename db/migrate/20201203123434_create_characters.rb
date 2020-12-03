661c39ea3180d9028197a52b171191c2a631fb00
Jakob Persons <jakobep24@gmail.com>
2020-12-03 07:51:49 -0500

    migrations and tables built

---
+++ db/migrate/20201203123434_create_characters.rb
@@ -0,0 +1,12 @@
+class CreateCharacters < ActiveRecord::Migration[6.0]
+  
+  def change
+
+    create_table :characters do |t|
+      t.integer :actor_id
+      t.integer :show_id
+    end
+
+  end
+
+end
